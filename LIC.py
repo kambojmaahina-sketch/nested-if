32ca94f0ab888afe4d4b67a7a083b5366641344e
Maahina <kambojmaahina@gmail.com>
2026-06-27 19:30:03 +0530

    LICeligibility

---
+++ LIC.py
@@ -0,0 +1,8 @@
+age=int(input("ente age:"))
+if age>=18:
+   if age<=45:
+       print("You are eligible for LIC.")
+   else:
+       print("You are not eligible for LIC.")
+else:
+    print("You are not eligible for LIC.")
